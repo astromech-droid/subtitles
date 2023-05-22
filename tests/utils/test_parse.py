b0029aa2273b8b03c44b5450972bcf20a23e4a44
astromech_droid <n.yamamoto.py@gmail.com>
2023-05-23 05:46:59 +0900

    test_parse.pyの階層を変更

--- tests/utils/test_parse.py
+++ tests/utils/test_parse.py
@@ -1,12 +1,17 @@
 import os
 
 import settings as s
+
 from utils import parse
 
 
 def test_remove_linenumbers():
-    test_input_file = os.path.join(s.TEST_DATA_DIR, "remove_linenumbers_before.vtt")
-    test_output_file = os.path.join(s.TEST_DATA_DIR, "remove_linenumbers_after.vtt")
+    test_input_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/remove_linenumbers_before.vtt"
+    )
+    test_output_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/remove_linenumbers_after.vtt"
+    )
 
     with open(test_input_file, "r", encoding="utf-8-sig") as f_input:
         test_input = f_input.readlines()
@@ -18,8 +23,12 @@ def test_remove_linenumbers():
 
 
 def test_remove_headers():
-    test_input_file = os.path.join(s.TEST_DATA_DIR, "remove_headers_before.vtt")
-    test_output_file = os.path.join(s.TEST_DATA_DIR, "remove_headers_after.vtt")
+    test_input_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/remove_headers_before.vtt"
+    )
+    test_output_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/remove_headers_after.vtt"
+    )
 
     with open(test_input_file, "r", encoding="utf-8-sig") as f_input:
         test_input = f_input.readlines()
@@ -31,8 +40,12 @@ def test_remove_headers():
 
 
 def test_remove_blanklines():
-    test_input_file = os.path.join(s.TEST_DATA_DIR, "remove_blanklines_before.vtt")
-    test_output_file = os.path.join(s.TEST_DATA_DIR, "remove_blanklines_after.vtt")
+    test_input_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/remove_blanklines_before.vtt"
+    )
+    test_output_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/remove_blanklines_after.vtt"
+    )
 
     with open(test_input_file, "r") as f_input:
         test_input = f_input.readlines()
@@ -44,8 +57,12 @@ def test_remove_blanklines():
 
 
 def test_extruct_starttime():
-    test_input_file = os.path.join(s.TEST_DATA_DIR, "extruct_starttime_before.vtt")
-    test_output_file = os.path.join(s.TEST_DATA_DIR, "extruct_starttime_after.vtt")
+    test_input_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/extruct_starttime_before.vtt"
+    )
+    test_output_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/extruct_starttime_after.vtt"
+    )
 
     with open(test_input_file, "r") as f_input:
         test_input = f_input.readlines()
@@ -57,8 +74,12 @@ def test_extruct_starttime():
 
 
 def test_put_starttime_on_alllines():
-    test_input_file = os.path.join(s.TEST_DATA_DIR, "put_starttime_on_alllines_before.txt")
-    test_output_file = os.path.join(s.TEST_DATA_DIR, "put_starttime_on_alllines_after.txt")
+    test_input_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/put_starttime_on_alllines_before.txt"
+    )
+    test_output_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/put_starttime_on_alllines_after.txt"
+    )
 
     with open(test_input_file, "r") as f_input:
         test_input = f_input.readlines()
@@ -70,8 +91,12 @@ def test_put_starttime_on_alllines():
 
 
 def test_merge_multilines():
-    test_input_file = os.path.join(s.TEST_DATA_DIR, "merge_multilines_before.txt")
-    test_output_file = os.path.join(s.TEST_DATA_DIR, "merge_multilines_after.txt")
+    test_input_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/merge_multilines_before.txt"
+    )
+    test_output_file = os.path.join(
+        s.TEST_DATA_DIR, "utils/parse/merge_multilines_after.txt"
+    )
 
     with open(test_input_file, "r") as f_input:
         test_input = f_input.readlines()
