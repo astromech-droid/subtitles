d913b6e9f44e7fd2c3af85a29806499ea683f033
astromech_droid <n.yamamoto.py@gmail.com>
2023-05-24 02:28:20 +0900

    本番用のurlを追加

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 from subtitles import subtitles
 
-url = "https://sample.com/test.vtt"
-title = "disneyplus_s1e1"
+# url = "https://ipv4-c001-itm002-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=147&e=1684903962&t=Oa-UVZdVgnXnzABY4BrohJVAJZY9ZWg8rNXPH4FkzLqkPIcsEDrDeeM38UKxh3UCAAWnhCN7V91u6b-BjftRe_21rUpeBf2U_Cm8oBDTEGDAMaGspnaFmUklzP5_WHvb9HKc08BuOqU-hj6Jz1LD_oycn1V7Cu5CEIfyzW2-IptJ4XH_HZGJi1g-g34nK_xvrj1zA_QcNY8dAImWGu2Bfx4JHX66XHA3BpzgNElca67tbK9v99mrIQ"
+# title = "glitch_techs_s1e1"
+
+# url = "https://ipv4-c001-itm002-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=147&e=1684904231&t=ON6kLDN2ulH4rSzVt0WV7V9xD0DI38j_md1Hbui8uKrWDzIiYo2L_tWUGkbbVYYwEvstI5JhBcw2Ii0GAkNxIhO_n4J4io8YTBqDT12FiYrqI8hLbfgLcuujrThWIyDqsyQ4IRqQBt-X5C3SiTGcBNdHRl9edkhxCmLKAd0kP2IV4bL8HRnzYwBDEkv_Eo1frlfjgHz2jHE5Ep4BSL1PTUAGCGybSChrn7vexBCKGBnMBTo"
+# title = "hilda_s1e1"
+
+# url = "https://ipv4-c003-itm002-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=98&e=1684904328&t=Gr1Vq_nn5oU7DsSqCZ-ah9qqMxKo8cnAdUKz8rAJeaHRrri8HZ3YbdFVc1pWK8Y4UTBlNhW6vD0rQ_ieEd4y3ca1plooOGKzFhk4oeGRff6CQ6K9WQ_Jpiq_bKEu3jaKK0sMbMI-PTlWjYSKOJFnfHPTzwSkE0GJr0aqPqCaUsZf5Lcnu6O8CzL_sb_n7NCaE49GpmNfcchd-37sTA4ygeWSyS4bgQ2qDY5wmqoct5usSI24mukp-w"
+# title = "kid_cosmic_s1e1"
+
+# url = "https://ipv4-c002-itm002-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=136&e=1684904501&t=7nQ8hRv3TMOgg5umr7Q8esETN61OY8iGikd-vpTpTp2hhISK9F75BIpyA439kThSwDfOiZ0q_aHgWGjd5aXzkkdhyTdSgOA1CyoEakj-xFV5Icl_ZjCjde2rafrJ7e_4tpr2QdEqmhoqn6Kz_fiJuxy58glJncblcc3-kLakfphUO5658_UyLvVD8RknPiGjw4hmIhDI4Hfp0eqUwajZp2MOOXZyBuy4amrmkxQSpLyH7eiRDq8tYg"
+# title = "kipo_s1e1"
+
+# url = "https://ipv4-c003-itm001-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=136&e=1684904598&t=bV4U718aMyuZvsgBs8ibytTxDfZVR8OA6XNgAVH5l7n7j3OVyq1AuHKOZn-WP9vLpkGa6G5P_MyY2tzXwX12-iwtmwCk3Upv8nqR217A_K6Y5nUcv49-bFvmpxm_W8CDu42cOHIiKRO-s6cDWEcs-POSGZP1n7lSqP43DFKMZycZ3OArjJX-xSevH9EkIJYB4S8t3cOkjSlmD8THMxt3z1pxREUj3AVVEuxpUF67J5ElNOU"
+# title = "little_witch_academia_s2e18"
+
+# url = "https://ipv4-c004-itm001-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=99&e=1684904676&t=9fI6qcO6148K9kka3bXe2EV2vnDatOZLm6n0mq7U8wTNpEEeLLtl1MzSGFs-Hy3SS4DGWGfMFEiihHZ02VVbx5_5oTGialiq4-Ns3HU3yZsCgoKR6YbdvpsQyMdX1Of3DIYNeTFcxOhui879y4JMfpvG-gbKah48WHUfKJo9_O5qRL0Mb77ia_Ea6QD_eT9ZVNrq_Z7csZRuckDDCHEdzBQyTcVtZzefHeBWFDT_UWl830nyxfpGzoo"
+# title = "romantic_killer_s1e1"
+
+# url = "https://ipv4-c004-itm001-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=99&e=1684904749&t=2H5d_ioKgzB-XAyCdb-hI7Kf_DPVfS3i2GBeQXNOTIpOH9FWEOmLIWpMehzfmAjNFZgwhJtgXzuBqv8FYODk8dKMAZBh2KMWOuR3ogNejHTCIvBr865tpVcZwnWft2fZUg_xZUX2HCKdFrON9KI5Jv8v65Kpl5RBmd32rhpN_oBMLfV57I0tBCYBstHv10omfmdgw420SIPqK2grCGdpIzg1xQjSWLJ72JyESFR3KqhNLxU"
+# title = "romantic_killer_s1e2"
+
+# url = "https://ipv4-c004-itm001-k-opticom-isp.1.oca.nflxvideo.net/?o=1&v=99&e=1684904820&t=Uc4HZrwPTutF5W0ttWMbQkaJ_KMwc7UxfQCnfXL350QYyopnQLRtw47iW3rLeZ2KQkOxY7aPCjWcoCPl2DR0VzW9jJrlyTImEFg_veZiamF1zSIE7rVWSmRDjra4u9EDEr_I75kUZ10plBFkcHjPGZacBwlQfNoyMF7Guub5ekaUImqSgvINMXESSgO5Vo1mVxz3aYOTUHBoeoZwMjrqTOnJpmZPt66qkHniwhGRuddALygpk4rLL2I"
+# title = "romantic_killer_s1e3"
+
+# url = "https://vod-akc-ap-north-1.media.dssott.com/ps01/disney/e6a5d957-222a-485b-974a-1cdb3e9295a9/r/1bf93eac-71ed-4039-a071-3ade818c6fd6/4b9e-MAIN/06/subtitles_1/seg_00000.vtt"
+# title = "zootopia_plus_s1e1"
+
+# url = "https://vod-akc-ap-north-1.media.dssott.com/ps01/disney/cca28bc2-eb49-476a-af99-173d06cfafc8/r/bf6f3bfc-01e1-4270-b605-264de963aab0/3737-MAIN/06/subtitles_1/seg_00000.vtt"
+# title = "zootopia_plus_s1e2"
+
+# url = "https://vod-akc-ap-north-1.media.dssott.com/ps01/disney/94dda514-70f4-4ebe-accd-457a2fa3b43b/r/4858c23f-b09d-4e38-89f7-143194a4414d/2e60-MAIN/06/subtitles_1/seg_00000.vtt"
+# title = "monsters_inc"
+
+url = ""
+title = ""
 
 pathes = subtitles.run(url, title)
 
--- subtitles/subtitles.py
+++ subtitles/subtitles.py
@@ -10,7 +10,13 @@ def _get_engine(url: str) -> Xxx2txt:
     if re.match(settings.TEST_XML_URL, url):
         return Xml2txt
 
+    elif re.match(r".*nflxvideo.net/.*", url):
+        return Xml2txt
+
     elif re.match(settings.TEST_VTT_URL, url):
+        return Vtt2txt
+
+    elif re.match(r".*/disney/.*/seg_\d{5}\.vtt$", url):
         return Vtt2txt
 
     else:
