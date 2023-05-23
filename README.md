8f81dc9e24e3e54326b2e10a0400da7982d92462
astromech_droid <n.yamamoto.py@gmail.com>
2023-05-23 18:26:09 +0900

    READMEにusageを記載

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
+# Usage
+```python
+from subtitles.core.Subtitles import Subtitles
+
+url = "https://sample.com/test.vtt"
+title = "disneyplus_s1e1"
+
+subtitles = Subtitles()
+pathes = subtitles.run(url, title)
+
+for path in pathes:
+    print(path)
+```
+
 # Tips
 #### How to download subtitles from Netflix
 * Login to netflix
--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-from subtitles.conf import settings
 from subtitles.core.Subtitles import Subtitles
 
+url = "https://sample.com/test.vtt"
+title = "disneyplus_s1e1"
+
 subtitles = Subtitles()
-
-# DisneyPlus: vtt
-url: str = settings.TEST_VTT_URL
-title: str = "disneyplus_s1e1"
-
 pathes = subtitles.run(url, title)
-for path in pathes:
-    print(path)
 
-
-# Netflix: xml
-url: str = settings.TEST_XML_URL
-title: str = "netflix_s1e1"
-
-pathes = subtitles.run(url, title)
 for path in pathes:
     print(path)
