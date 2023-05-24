8f52c0308e4b9113be446a2e528901cd6faf338d
astromech_droid <n.yamamoto.py@gmail.com>
2023-05-25 02:45:06 +0900

    xml2txt実装完了

---
+++ main.py
@@ -0,0 +1,32 @@
+import glob
+import os
+
+from subtitles import xml2txt
+from subtitles.conf import settings
+
+title: str = "kid_cosmic_s1e1"
+dirname: str = os.path.join(settings.XML_DIR, title)
+
+pathes: list[str] = glob.glob(f"{dirname}/*")
+path: str = pathes[0]
+lines: list[list[str]] = xml2txt.read(path)
+
+for starttime, text in lines:
+    print(f"{starttime}: {text}")
+
+
+# Usage - download -
+"""
+from subtitles.conf import settings
+from subtitles.Downloader import Downloader
+
+service = settings.SERVICE_DISNEYPLUS
+downloader = Downloader(service)
+title = "test"
+
+url: str = "https://vod-akc-ap-north-1.media.dssott.com/ps01/disney/e6a5d957-222a-485b-974a-1cdb3e9295a9/r/1bf93eac-71ed-4039-a071-3ade818c6fd6/4b9e-MAIN/06/subtitles_1/seg_00000.vtt"
+urls: list[str] = downloader.get_urls(url)
+dirname: str = downloader.get_dirname(title)
+
+downloader.download_all(urls, dirname)
+"""
---
+++ subtitles/xml2txt.py
@@ -0,0 +1,71 @@
+import math
+import os
+import re
+
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString, Tag
+
+
+def _get_starttime(element) -> str:
+    starttime: str = element.get("begin")  # ex. 8050000000t
+    seconds: float = int(re.match(r"^(\d+)t", starttime)[1]) / 10000000
+
+    h = math.floor(seconds / 3600)
+    m = math.floor(seconds / 60)
+    s = math.floor(seconds - (h * 3600 + m * 60))
+    ms = "{:.3f}".format(seconds - math.floor(seconds))[2:]
+
+    return f"{str(h).zfill(2)}:{str(m).zfill(2)}:{str(s).zfill(2)}.{ms}"
+
+
+def _get_text(element) -> str:
+    def _recurse(element) -> list[str]:
+        _text: list[str] = []
+
+        for c in element.contents:
+            if type(c) is NavigableString:
+                if c.text != "":
+                    _text.append(c.text)
+
+            elif type(c) is Tag:
+                if c.text != "":
+                    _: str = " ".join(_recurse(c))
+                    _text.append(_)
+
+        return _text
+
+    text: list[str] = _recurse(element)
+
+    return " ".join(text)
+
+
+def parse(xml_doc: str) -> list[list[str]]:
+    lines: list[list[str]] = []
+
+    soup = BeautifulSoup(xml_doc, "xml")
+    elements = list(soup.find_all("p"))  # NavigableString, Tag
+
+    for el in elements:
+        starttime: str = _get_starttime(el)
+        text: str = _get_text(el)
+        line: list[str] = [starttime, text]
+        lines.append(line)
+
+    return lines
+
+
+def read(path: str) -> list[str]:
+    with open(path, "r", encoding="utf-8-sig") as f:
+        lines: list[str] = f.read()
+
+    return parse(lines)
+
+
+def save(path: str, lines: list[str]) -> None:
+    dirname: str = os.path.dirname(path)
+
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    with open(path, "w") as f:
+        f.writelines(lines)
