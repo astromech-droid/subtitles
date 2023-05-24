70a1228922354a5bdfb1a9c2dea4dd52069105c3
astromech_droid <n.yamamoto.py@gmail.com>
2023-05-24 19:17:49 +0900

    リファクタリングのため全体的に一旦削除

--- subtitles/conf/settings.py
+++ subtitles/conf/settings.py
@@ -1,27 +1,21 @@
 import os
 
+# DIRECTRIES
 OUTPUT_DIR = "output"
 VTT_DIR = os.path.join(OUTPUT_DIR, "vtt")
 XML_DIR = os.path.join(OUTPUT_DIR, "xml")
 TXT_DIR = os.path.join(OUTPUT_DIR, "txt")
-
 TEST_DIR = "tests"
 TEST_DATA_DIR = os.path.join(TEST_DIR, "data")
 
-TEST_VTT_URL = (
-    "https://raw.githubusercontent.com/astromech-droid/subtitles/main/tests"
-    "/data/utils/http/vtt/disneyplus/seg_00000.vtt"
-)
+# FILENAMES
+DEFAULT_TXT_FILENAME = "subtitles.txt"
+DEFAULT_XML_FILENAME = "subtitles.xml"
+DEFAULT_VTT_FILENAME = "subtitles.vtt"
 
-TEST_XML_URL = (
-    "https://raw.githubusercontent.com/astromech-droid/subtitles/main/tests"
-    "/data/utils/http/xml/netflix/subtitles.xml"
-)
-
+# SERVICES
 SERVICE_DISNEYPLUS = "disneyplus"
 SERVICE_NETFLIX = "netflix"
 
+# PARAMETERS
 MAX_SEGMENTS = 75  # 3h = 360min, 5min/vtt, 360/5 = 75vtt
-
-DEFAULT_TXT_FILENAME = "subtitles.txt"
-DEFAULT_XML_FILENAME = "subtitles.xml"
