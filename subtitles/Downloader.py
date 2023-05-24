44465c5d5870dab8791954220fbf538bfe66f3c9
astromech_droid <n.yamamoto.py@gmail.com>
2023-05-24 19:18:06 +0900

    Download機能実装完了

---
+++ subtitles/Downloader.py
@@ -0,0 +1,66 @@
+import os
+import re
+import time
+
+import requests
+
+from subtitles.conf import settings
+
+
+class Downloader:
+    def __init__(self, service: str):
+        self.service = service
+        pass
+
+    def _fetch(self, url: str) -> str:
+        response = requests.get(url)
+        return response.text
+
+    def _save(self, text: str, path: str) -> None:
+        with open(path, "w") as f:
+            f.write(text)
+
+    def _get_urls(self, url: str) -> list[str]:
+        urls = []
+
+        if self.service == settings.SERVICE_DISNEYPLUS:
+            for i in range(0, settings.MAX_SEGMENTS):
+                url_head = re.match(r"(^https://.*/)seg_\d{5}.vtt", url)[1]
+                seg_filename = f"seg_{str(i).zfill(5)}.vtt"
+                urls.append(f"{url_head}{seg_filename}")
+
+        elif self.service == settings.SERVICE_NETFLIX:
+            urls.append(url)
+
+        return urls
+
+    def download(self, url: str, path: str) -> None:
+        text: str = self._fetch(url)
+        self._save(text, path)
+
+    def get_dirname(self, title: str) -> str:
+        if self.service == settings.SERVICE_DISNEYPLUS:
+            dirname: str = os.path.join(settings.VTT_DIR, title)
+
+        elif self.service == settings.SERVICE_NETFLIX:
+            dirname: str = os.path.join(settings.XML_DIR, title)
+
+        return dirname
+
+    def download_all(self, urls: list[str], dirname: str) -> list[str]:
+        pathes: list[str] = []
+
+        for url in urls:
+            if self.service == settings.SERVICE_DISNEYPLUS:
+                filename: str = re.match(r"^https://.*/(seg.*.vtt)$", url)[1]
+
+            elif self.service == settings.SERVICE_NETFLIX:
+                filename: str = settings.DEFAULT_XML_FILENAME
+
+            path: str = os.path.join(dirname, filename)
+            self.download(url, path)
+            pathes.append(path)
+
+            time.sleep(0.5)
+
+        return pathes
---
+++ tests/test_downloader.py
@@ -0,0 +1,61 @@
+import filecmp
+import os
+import re
+
+from subtitles.conf import settings
+from subtitles.Downloader import Downloader
+
+
+def test_download(tmp_path):
+    path: str = tmp_path / "test.txt"
+    url: str = "https://raw.githubusercontent.com/astromech-droid/subtitles/main/LICENSE"
+    service: str = settings.SERVICE_DISNEYPLUS
+    Downloader(service).download(url, path)
+
+    with open(path, "r") as f:
+        downloaded: str = f.read()
+
+    with open("./LICENSE", "r") as g:
+        expected: str = g.read()
+
+    assert downloaded == expected
+
+
+def test_get_urls():
+    url: str = "https://this/is/a/test/seg_00000.vtt"
+    service: str = settings.SERVICE_DISNEYPLUS
+    urls: list[str] = Downloader(service)._get_urls(url)
+
+    for _url in urls:
+        if not re.match(r"^https://.*/seg_\d{5}\.vtt$", _url):
+            assert False
+
+
+def test_get_dirname():
+    service: str = settings.SERVICE_DISNEYPLUS
+    title: str = "test"
+    expected: str = os.path.join(settings.VTT_DIR, title)
+
+    assert Downloader(service).get_dirname(title) == expected
+
+
+def test_download_all(tmp_path):
+    urls: list[str] = [
+        "https://raw.githubusercontent.com/astromech-droid/subtitles/main/tests/data/utils/http/vtt/disneyplus/seg_00000.vtt",
+        "https://raw.githubusercontent.com/astromech-droid/subtitles/main/tests/data/utils/http/vtt/disneyplus/seg_00001.vtt",
+    ]
+    service: str = settings.SERVICE_DISNEYPLUS
+    dirname: str = tmp_path._str
+    Downloader(service).download_all(urls, dirname)
+
+    if not filecmp.cmp(
+        os.path.join(dirname, "seg_00000.vtt"),
+        os.path.join(settings.TEST_DATA_DIR, "downloader/seg_00000.vtt"),
+    ):
+        assert False
+
+    if not filecmp.cmp(
+        os.path.join(dirname, "seg_00001.vtt"),
+        os.path.join(settings.TEST_DATA_DIR, "downloader/seg_00001.vtt"),
+    ):
+        assert False
