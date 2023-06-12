b41bf0232bcaade916148765d946351850bec2fa
astromech_droid <n.yamamoto.py@gmail.com>
2023-06-12 17:38:33 +0900

    Netflixのmergeに失敗する問題を修正

--- cli/merger.py
+++ cli/merger.py
@@ -10,17 +10,19 @@ class Merger:
     def merge(self, dirname_src, path_dst: str) -> str:
         filenames: str = sorted(os.listdir(dirname_src))
 
-        if os.path.exists(path_dst):
-            os.remove(path_dst)
-
-        for filename in filenames:
+        for i, filename in enumerate(filenames):
             if filename != settings.DEFAULT_TXT_FILENAME:
                 path_src: str = os.path.join(dirname_src, filename)
 
                 with open(path_src, "r") as f:
                     text = f.read()
 
-                with open(path_dst, "a") as g:
-                    g.write(text)
+                if i == 0:
+                    with open(path_dst, "w") as g:
+                        g.write(text)
+
+                else:
+                    with open(path_dst, "a") as g:
+                        g.write(text)
 
         return path_dst
