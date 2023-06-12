6d01a27d95a77f879b8305a4bda3d10d3fe1cd84
astromech_droid <n.yamamoto.py@gmail.com>
2023-06-13 00:26:00 +0900

    検索機能を追加

--- app/views.py
+++ app/views.py
@@ -1,8 +1,5 @@
-import os
-
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
-from subtitles import settings
 
 from app.models import Episode, Line
 
@@ -30,3 +27,32 @@ def lines(request, title):
     context = {"lines": lines}
 
     return HttpResponse(template.render(context, request))
+
+
+def search(request):
+    template = loader.get_template("app/search.html")
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
+
+def search_api(request):
+    regex_text: str = request.GET["regex_text"]
+    # lines = Line.objects.filter(text=text)
+    lines = Line.objects.filter(text__iregex=regex_text)
+    context: dict = _serialize_lines(lines)
+
+    return JsonResponse(context)
+
+
+def _serialize_lines(lines) -> dict:
+    context = {"lines": []}
+    for line in lines:
+        values = {
+            "text": line.serializable_value("text"),
+            "timestamp": line.serializable_value("timestamp"),
+            "episode_title": line.episode.title,
+        }
+        context["lines"].append(values)
+
+    return context
