1732ea15dec40cebc0e2af791d288e8dd13fd1b5
astromech_droid <n.yamamoto.py@gmail.com>
2023-06-18 17:22:36 +0900

    episodeの一覧ページを作成

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("episodes/", views.episodes, name="episodes"),
     path("api/search/", views.search, name="search"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
-from app.models import Line
+from app.models import Episode, Line
 
 
 def index(request):
     template = loader.get_template("app/index.html")
-    message = "HelloWorld"
-    context = {
-        "message": message,
-    }
+    context = {}
 
+    return HttpResponse(template.render(context, request))
+
+
+def episodes(request):
+    template = loader.get_template("app/episodes.html")
+    episodes = Episode.objects.all()
+    context = {"episodes": episodes}
     return HttpResponse(template.render(context, request))
 
 
